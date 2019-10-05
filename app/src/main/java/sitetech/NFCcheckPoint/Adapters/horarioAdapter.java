474a039cf254fcd53cc88947f4390b5d70ec7660
William <williamvaldez@outlook.com>
2019-10-05 15:58:30 -0600

    Agregado horario y ruta

---
+++ app/src/main/java/sitetech/NFCcheckPoint/Adapters/horarioAdapter.java
@@ -0,0 +1,130 @@
+package sitetech.NFCcheckPoint.Adapters;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.omega_r.libs.omegarecyclerview.OmegaRecyclerView;
+import com.omega_r.libs.omegarecyclerview.swipe_menu.SwipeViewHolder;
+
+import java.util.List;
+
+import sitetech.NFCcheckPoint.AppController;
+import sitetech.NFCcheckPoint.Helpers.Dialog;
+import sitetech.NFCcheckPoint.Helpers.ToastHelper;
+import sitetech.NFCcheckPoint.Helpers.myDialogInterface;
+import sitetech.NFCcheckPoint.db.Horario;
+import sitetech.NFCcheckPoint.db.HorarioDao;
+import sitetech.routecheckapp.R;
+
+public class horarioAdapter extends OmegaRecyclerView.Adapter<horarioAdapter.ViewHolder> {
+    public List<Horario> lista;
+    private onItemClick onItemClick;
+
+
+    public horarioAdapter(List<Horario> l, onItemClick onclick) {
+        lista = l;
+        this.onItemClick = onclick;
+    }
+
+    @Override
+    public int getItemCount() {
+        return lista.size();
+    }
+
+    @Override
+    public horarioAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new horarioAdapter.ViewHolder(parent);
+    }
+
+    @Override
+    public void onBindViewHolder(horarioAdapter.ViewHolder holder, int position) {
+        Horario dato = lista.get(position);
+        holder.display(dato);
+    }
+
+    public void updateData(Horario bx) {
+        boolean nuevo = true;
+        for (Horario rx : lista) {
+            if (rx.getId() == bx.getId()) {
+                lista.set(lista.indexOf(rx), bx);
+                nuevo = false;
+            }
+            ToastHelper.info("Se a modificado el horario.");
+        }
+
+        if (nuevo) {
+            lista.add(bx);
+            ToastHelper.exito("Se a creado el horario.");
+        }
+
+        notifyDataSetChanged();
+    }
+
+    public void deleteData(Horario rx) {
+        ToastHelper.normal("Se a eliminado el horario " + rx.getHora());
+        lista.remove(rx);
+        notifyDataSetChanged();
+    }
+
+    public class ViewHolder extends SwipeViewHolder implements View.OnClickListener {
+        private final TextView thora;
+        private final TextView thoraFestivo;
+        private final ImageView beliminar;
+        HorarioDao horarioManager = AppController.daoSession.getHorarioDao();
+
+        private Horario currentItem;
+
+        public ViewHolder(ViewGroup itemView) {
+            super(itemView, R.layout.horario_template, SwipeViewHolder.NO_ID, R.layout.swipe_menu);
+
+            thora = (findViewById(R.id.thora));
+            thoraFestivo = (findViewById(R.id.thoraFestivo));
+            beliminar = (findViewById(R.id.beliminar));
+            beliminar.setOnClickListener(this);
+
+            contentView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //smoothOpenLeftMenu();
+                    onItemClick.onClickItemList(v, getAdapterPosition());
+                }
+            });
+        }
+
+        @Override
+        public void onClick(final View v) {
+            switch (v.getId()) {
+                case R.id.beliminar:
+                    Dialog.showAsk2(v, "Eliminar Ruta", "¿Desea realmente eliminar esta ruta?",
+                            "Eliminar", "Cancelar", new myDialogInterface() {
+                                @Override
+                                public View onBuildDialog() {
+                                    return null;
+                                }
+
+                                @Override
+                                public void onCancel() {
+                                    smoothCloseMenu();
+                                }
+
+                                @Override
+                                public void onResult(View vista) {
+                                    currentItem.setEliminado(true);
+                                    horarioManager.update(currentItem);
+                                    deleteData(currentItem);
+                                    smoothCloseMenu();
+                                }
+                            });
+                    break;
+            }
+        }
+
+        public void display(Horario rx) {
+            currentItem = rx;
+            thora.setText(rx.getHora() + " Min Max: " + rx.getMaxMinutos());
+            thoraFestivo.setText(rx.getHoraFestivo() + " Min Max: " + rx.getMaxMinutosFestivo());
+        }
+    }
+}
---
+++ app/src/main/java/sitetech/NFCcheckPoint/ui/horarios/HorariosFragment.java
@@ -0,0 +1,101 @@
+package sitetech.NFCcheckPoint.ui.horarios;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.omega_r.libs.omegarecyclerview.OmegaRecyclerView;
+
+import java.io.Serializable;
+import java.util.List;
+
+import sitetech.NFCcheckPoint.Adapters.horarioAdapter;
+import sitetech.NFCcheckPoint.Adapters.onItemClick;
+import sitetech.NFCcheckPoint.Adapters.rutaAdapter;
+import sitetech.NFCcheckPoint.AppController;
+import sitetech.NFCcheckPoint.Helpers.activityHelper;
+import sitetech.NFCcheckPoint.db.Horario;
+import sitetech.NFCcheckPoint.db.HorarioDao;
+import sitetech.NFCcheckPoint.db.Ruta;
+import sitetech.NFCcheckPoint.db.RutaDao;
+import sitetech.NFCcheckPoint.ui.rutas.RutaAgregarFragment;
+import sitetech.NFCcheckPoint.ui.rutas.RutasFragment;
+import sitetech.routecheckapp.R;
+
+public class HorariosFragment extends Fragment implements Serializable {
+    private View vista;
+    private Button bagregar;
+    private OmegaRecyclerView ulista;
+    private TextView lnotificar;
+    private horarioAdapter dataAdapter;
+    private HorariosFragment fragmento;
+    public Horario Itemseleccionado;
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.horarios_fragment, container, false);
+        bagregar = root.findViewById(R.id.bagregar);
+        ulista = root.findViewById(R.id.ulista);
+        lnotificar = root.findViewById(R.id.lnotificar);
+        Itemseleccionado = null;
+
+        vista = root;
+        fragmento = this;
+        bagregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Itemseleccionado = null;
+                activityHelper.cargarFragmento(fragmento, new RutaAgregarFragment());
+            }
+        });
+
+        showList();
+
+        return root;
+    }
+
+    private void showList(){
+        HorarioDao horarioDao = AppController.daoSession.getHorarioDao(); // QUERY PARA OBTENER TODOS MENOS LOS ELIMINADOS
+        List<Horario> lista = horarioDao.queryBuilder()
+                .where(HorarioDao.Properties.Eliminado.eq(false))
+                .list();
+
+        dataAdapter = new horarioAdapter(lista, new onItemClick() {
+            @Override
+            public void onClickItemList(View v, int position) {
+                fragmento.Itemseleccionado = dataAdapter.lista.get(position);
+                activityHelper.cargarFragmento(fragmento, new HorarioAgregarFragment());
+            }
+        });
+
+        /*for ( Bus b : BusDao.loadAll() ) {
+            Log.d(b.getPlaca(), b.getInterno());
+        }*/
+
+        if (dataAdapter.getItemCount() == 0) {
+            ulista.setVisibility(View.GONE);
+            lnotificar.setVisibility(View.VISIBLE);
+        }
+        else {
+            ulista.setVisibility(View.VISIBLE);
+            lnotificar.setVisibility(View.GONE);
+        }
+
+
+        ulista.setLayoutManager(new LinearLayoutManager(getContext()));
+        ulista.setAdapter(dataAdapter);
+    }
+
+
+    public void updateList(Horario bx){
+        dataAdapter.updateData(bx);
+    }
+}
